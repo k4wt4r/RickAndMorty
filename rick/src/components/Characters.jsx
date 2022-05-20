177429189c316f4384a77f91592e0f4f01737c23
k4wt4r <kawtarelalmi0@gmail.com>
2022-05-20 15:04:10 +0100

    feat: added search bar

--- rick/src/components/Characters.jsx
+++ rick/src/components/Characters.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import Card from "./Card/Card";
 import Cardstyle from "../styles/scss/Cardstyle.module.scss";
 
-function Characters() {
+function Characters({ character }) {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -13,13 +14,31 @@ function Characters() {
     })();
   }, []);
   return (
-    <div className={Cardstyle.charactersList}>
-      {data.map((character, index) => (
-        <Card character={character} key={index} />
-      ))}
+    <div className={Cardstyle.characterspage}>
+      <input
+        type="search"
+        placeholder="Search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className={Cardstyle.charactersList}>
+        {data
+          .filter((character) => {
+            if (search === "") {
+              return character;
+            } else if (
+              character.name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return character;
+            }
+          })
+          .map((character, index) => (
+            <div key={index} className={Cardstyle.characterslist}>
+              <Card character={character} />
+            </div>
+          ))}
+      </div>
     </div>
   );
-  // <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
 
 export default Characters;
